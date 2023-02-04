2a560aa9ea7d3874baee2110728a4232e3dd4eb2
Wali Muhammad <luckywali0986@gmail.com>
2023-02-04 21:28:12 +0500

    Create store, connect redux with react and displayed all data

---
+++ src/redux/configureStore.js
@@ -0,0 +1,13 @@
+import { configureStore } from "@reduxjs/toolkit";
+import missionsReducer from "./mission/Misson";
+
+
+const rootreducer = {
+  missions: missionsReducer,
+};
+
+const store = configureStore({
+  reducer: rootreducer,
+});
+
+export default store;
