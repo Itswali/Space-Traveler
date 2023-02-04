c19d1a9eadfaa123be5e157b59fbf4094921d030
Wali Muhammad <luckywali0986@gmail.com>
2023-02-04 21:27:06 +0500

    use api to get data of mission

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,10 +1,34 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { addMissions } from '../redux/mission/Misson';
 
-export default function Missions() {
+const Missions = () => {
+  const dispatch = useDispatch();
+  const missions = useSelector(state => state.missions);
+
+  useEffect(() => {
+    if (missions.length === 0) {
+      fetch('https://api.spacexdata.com/v3/missions')
+        .then(res => res.json())
+        .then(data => {
+          dispatch(addMissions(data));
+        });
+    }
+  }, [dispatch, missions.length]);
+
   return (
     <div>
-      <h1> HELLO MISSIONS</h1>
-      
+      <h2>Missions</h2>
+      <ul>
+        {missions.map(mission => (
+          <li key={mission.mission_id}>
+            <h3>{mission.mission_name}</h3>
+            <p>{mission.description}</p>
+          </li>
+        ))}
+      </ul>
     </div>
-  )
-}
+  );
+};
+
+export default Missions;
