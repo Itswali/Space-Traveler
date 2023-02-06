99d24feb3912a6ae63a5391af2a5a8ed0a45e335
Wali Muhammad <luckywali0986@gmail.com>
2023-02-06 15:27:49 +0500

    get data from API

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import '../style/style.scss';
 
-export default function Profile() {
+const Profile = () => {
+  const missions = useSelector((state) => state.missions);
+  const joinedMissions = missions.filter((mission) => mission.reserved);
+
   return (
-    <div>
-      <h1>THIS IS MY PROFILE</h1>
+    <div className="profile-container">
+      <h2 className="profile-title">My Profile</h2>
+      <div className="profile-mission-container">
+        <div className="profile-mission-list-container">
+          <h3 className="profile-mission-list-title">Joined Missions:</h3>
+          <ul className="profile-mission-list">
+            {joinedMissions.map((mission) => (
+              <li className="profile-mission-item" key={mission.mission_id}>
+                {mission.mission_name}
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
     </div>
   );
-}
+};
+
+export default Profile;
