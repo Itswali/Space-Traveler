bcc934d6af5160b0209a033b16755aca20d646e6
hthantoo <hthant00chk@gmail.com>
2023-02-06 16:33:33 +0630

    Underline active link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,48 +1,36 @@
 import React from 'react';
-import { Link, useLocation } from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
 import logo from '../img/logo.png';
 import '../style/style.scss';
 
 export default function Navbar() {
-  const location = useLocation();
-
   return (
     <div>
       <nav className="navbar">
         <div className="head-icon">
           <li className="">
-            {' '}
             <Link to="/">
-              {' '}
               <img className="icon" src={logo} alt="logo" />
-              {' '}
             </Link>
-            {' '}
           </li>
           <h2> Space Travelers Hub </h2>
         </div>
         <ul className="links">
-          <li
-            className={`link ${location.pathname === '/Rockets' && 'active'}`}
-          >
-            {' '}
-            <Link to="/">Rockets</Link>
-            {' '}
+          <li>
+            <NavLink to="/" className="link">
+              Rockets
+            </NavLink>
           </li>
-          <li
-            className={`link ${location.pathname === '/Missions' && 'active'}`}
-          >
-            {' '}
-            <Link to="/Missions">Missions</Link>
-            {' '}
+          <li>
+            <NavLink to="/Missions" className="link">
+              Missions
+            </NavLink>
           </li>
           <span className="horizontal-divider"> </span>
-          <li
-            className={`link ${location.pathname === '/Profile' && 'active'}`}
-          >
-            {' '}
-            <Link to="/Profile">My Profile</Link>
-            {' '}
+          <li>
+            <NavLink to="/Profile" className="link">
+              My Profile
+            </NavLink>
           </li>
         </ul>
       </nav>
