fd447eace125898ae2d7cfb61ef5b57885cca741
Wali Muhammad <luckywali0986@gmail.com>
2023-02-06 13:07:09 +0500

    temporary

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,33 +10,7 @@ export default function Navbar() {
   return (
     <div>
       <nav className="navbar">
-        <div className="head-icon">
-          <li className="">
-            {' '}
-            <Link to="/">
-              {' '}
-              <img className="icon" src={logo} alt="logo" />
-              {' '}
-            </Link>
-            {' '}
-          </li>
-          <h2>  Space Travelers Hub </h2>
-        </div>
-        <ul className="links">
-          <img className="icon" src={logo} alt="logo" />
-          <h2>  Space Travelers Hub </h2>
-        </div>
-        <ul className="links">
-          <li className="link">
-            {' '}
-            <Link to="/Missions">Missions</Link>
-            {' '}
-          </li>
-          <li className="link">
-            {' '}
-            <Link to="/Space">SpaceShips</Link>
-            {' '}
-          </li>
+        
           <span className="horizontal-divider"> </span>
           <li className="link">
           <li className="">
