3bf1a3df31c7c9e27d30326b8b13b5862c6f4574
Wali Muhammad <luckywali0986@gmail.com>
2023-02-06 15:30:16 +0500

    implemented following feature add join mission button add leave mission button add status button member/not a member add defalut style for mission page

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,28 +1,9 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
-import '../style/style.scss';
 
-const Profile = () => {
-  const missions = useSelector((state) => state.missions);
-  const joinedMissions = missions.filter((mission) => mission.reserved);
-
+export default function Profile() {
   return (
-    <div className="profile-container">
-      <h2 className="profile-title">My Profile</h2>
-      <div className="profile-mission-container">
-        <div className="profile-mission-list-container">
-          <h3 className="profile-mission-list-title">Joined Missions:</h3>
-          <ul className="profile-mission-list">
-            {joinedMissions.map((mission) => (
-              <li className="profile-mission-item" key={mission.mission_id}>
-                {mission.mission_name}
-              </li>
-            ))}
-          </ul>
-        </div>
-      </div>
+    <div>
+      <h1>THIS IS MY PROFILE</h1>
     </div>
   );
-};
-
-export default Profile;
+}
